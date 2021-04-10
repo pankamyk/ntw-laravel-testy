e4f2cf30df149dd101a7037e8dc7b86febe51086
pankamyk <pankamyk11@gmail.com>
2021-04-11 01:12:05 +0200

    added admin middlevare

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,28 +1,18 @@
 @extends('layouts.app')
-
+ 
 @section('content')
 <div class="container">
    <div class="row justify-content-center">
       <div class="col-md-8">
          <div class="card">
-            <div class="card-header">{{ __('Dashboard') }}</div>
-
+            <div class="card-header">Dashboard</div>
+ 
             <div class="card-body">
-               @if (session('status'))
-                  <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                  </div>
-               @endif
-
                @if (auth()->user()->is_admin == 1)
                   <a href="{{url('admin/routes')}}">Admin</a>
                @else
-                  <div class=”panel-heading”>
-                     Normal User
-                  </div>
+                  <div class=”panel-heading”>Normal User</div>
                @endif
-
-               {{ __('You are logged in!') }}
             </div>
          </div>
       </div>
